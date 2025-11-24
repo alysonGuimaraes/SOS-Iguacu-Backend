451854fe63354475bbf4abb3d78271ed0e6ca71d
Miguel Wolff <miguelwolff191@gmail.com>
2025-11-24 02:03:04 -0300

    Integração com o Front e adicionado o cors-headers nos requisitos.

--- SOS_Iguacu_Backend/settings.py
+++ SOS_Iguacu_Backend/settings.py
@@ -145,3 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",  # O endereço padrão do React dev server
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
